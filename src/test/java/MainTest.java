2f7526a127484689dab276d4bcfae0d00b3c1bdd
elkhan199 <ismayilzadeelxan@gmail.com>
2021-03-26 10:56:51 +0900

    some tests are added

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,8 +1,31 @@
 import org.junit.Test;
-
+import org.junit.Assert;
 public class MainTest {
     @Test
-    public void testMain(){
+    public void testMainNullNull(){
+        try{
+            Main.main(new String[]{null,null});
+            Assert.fail();
+        }catch (Exception e){
 
+        }
+    }
+    @Test
+    public void testMainNullString(){
+        try{
+            Main.main(new String[]{null,"Adventure"});
+            Assert.fail();
+        }catch (Exception e){
+
+        }
+    }
+    @Test
+    public void testMainStringNull(){
+        try{
+            Main.main(new String[]{"Adventure",null});
+            Assert.fail();
+        }catch (Exception e){
+
+        }
     }
 }
