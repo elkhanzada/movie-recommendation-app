ac0bb3b8b7b8f52b5f64d32d283c5927559a8183
elkhan199 <ismayilzadeelxan@gmail.com>
2021-04-22 00:05:26 +0900

    more tests are added

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -41,6 +41,13 @@ public class MainTest {
         }catch (Exception e){
 
         }
+    }@Test
+    public void testEmptyEmptyEmptyEmpty(){
+        try{
+            Main.main(new String[]{"","","",""});
+        }catch (Exception e){
+
+        }
     }
     @Test
     public void testEmptyEmptyEmptyString(){
@@ -76,7 +83,7 @@ public class MainTest {
     @Test
     public void testEmptyGenre(){
         try{
-            Main.main(new String[]{"F","25","gradstudent",""});
+            Main.main(new String[]{"M","25","gradstudent",""});
         }catch (Exception e){
 
         }
@@ -93,6 +100,108 @@ public class MainTest {
     public void testLongGenre(){
         try{
             Main.main(new String[]{"F","25","gradstudent","asdfasdfafafadasfsdasdsdfddfhffuhsdgfidfhiuhfiufhfiusahiasuhdaisuhdaihasduadiauhdasi"});
+        }catch (Exception e){
+
+        }
+    }
+    @Test
+    public void testEmpyFullGenre(){
+        try{
+            Main.main(new String[]{"M","25","gradstudent","|Horror|"});
+        }catch (Exception e){
+
+        }
+    }
+    @Test
+    public void testEmptyOccup(){
+        try{
+            Main.main(new String[]{"F","25","",""});
+        }catch (Exception e){
+
+        }
+    }
+    @Test
+    public void testLongOccup(){
+        try{
+            Main.main(new String[]{"F","25","sasklasjfasdsadasfsafsafasfsafsafsafsfasfaskjflfkjasfklsfjaslkfjasflkjaasd"});
+        }catch (Exception e){
+
+        }
+    }
+    @Test
+    public void testUnknownOccup(){
+        try{
+            Main.main(new String[]{"F","25","aibar"});
+        }catch (Exception e){
+
+        }
+    }
+    @Test
+    public void testNullOccup(){
+        try{
+            Main.main(new String[]{"F","25","sasklasjfaskjflfkjasfklsfjaslkfjasflkjaasd",null});
+        }catch (Exception e){
+
+        }
+    }
+    @Test
+    public void testEmptyAge(){
+        try{
+            Main.main(new String[]{"M","","",""});
+        }catch (Exception e){
+
+        }
+    }
+    @Test
+    public void testLongAge(){
+        try{
+            Main.main(new String[]{"F","2523212311234112312123121241231241231124",""});
+        }catch (Exception e){
+
+        }
+    }
+    @Test
+    public void testStringAge(){
+        try{
+            Main.main(new String[]{"F","age",""});
+        }catch (Exception e){
+
+        }
+    }
+    @Test
+    public void testPossibleAges(){
+        try{
+            Main.main(new String[]{"F","1",""});
+            Main.main(new String[]{"F","18",""});
+            Main.main(new String[]{"F","25",""});
+            Main.main(new String[]{"F","35",""});
+            Main.main(new String[]{"F","45",""});
+            Main.main(new String[]{"F","55",""});
+            Main.main(new String[]{"F","56",""});
+        }catch (Exception e){
+
+        }
+    }
+    @Test
+    public void testEmptyGender(){
+        try{
+            Main.main(new String[]{"","25",""});
+        }catch (Exception e){
+
+        }
+    }
+    @Test
+    public void testLongGender(){
+        try{
+            Main.main(new String[]{"akfjaskfljflsakfjasflkaj","25",""});
+        }catch (Exception e){
+
+        }
+    }
+    @Test
+    public void testLessMovie(){
+        try{
+            Main.main(new String[]{"M","1","Doctor", "Horror"});
         }catch (Exception e){
 
         }
