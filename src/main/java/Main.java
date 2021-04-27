144ee32c08a79789272b1e99d4525836d3cd9242
Kasymzhan Abdyldayev <kasymzhan.abdyldayev@gmail.com>
2021-04-27 18:27:39 +0900

    Implemented error handling v1.0; General case errors are handled

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,7 @@ public class Main {
         HashMap<String, Integer> workID = new HashMap<>();
         Utils.setOccupationHash(workID); // now workID contains all mappings
 
+        //* Args check
         if (args.length < 3 || args.length > 4) {
             //-------------------------------------------------------------------
             //? are these messages enough for case with !=2 args?
@@ -19,127 +20,139 @@ public class Main {
             System.out.println("Please, pass exactly 3 or 4 arguments!");
             System.out.print("Try to remove spaces between occupations consisting of several words, such as ");
             System.out.println("\"college student\" -> \"collegestudent\"");
+            return;
         } 
-        else {
-            //Elkhan's code
-            gender = args[0].toLowerCase();
-            if (args[1].length() > 0)
-                age = Integer.parseInt(args[1]);
-            else
-                age = -1;
-            work = args[2].toLowerCase();
-            if (args.length == 4) {
-                gender = args[0].toLowerCase();
-                if (args[1].length() > 0)
-                    age = Integer.parseInt(args[1]);
-                else
-                    age = -1;
-                work = args[2].toLowerCase();
-                genres = args[3].toLowerCase().split("\\|");
-                //* START CHECK FOR GENRE INPUT
-                if (genres.length == 0) {
-                    System.out.println("Please enter valid input");
-                    return;
-                }
-                if (genres.length > 10) {
-                    //if too many genres
-                    System.out.println("Input for genre is too long, please try to include a genre not more that one time.");
-                    return;
-                }
-                boolean is_empty = true;
-                for (String genre : genres) {
-                    //if the genre string is too long
-                    if (genre.length() > 50) {
-                        System.out.println("There are no movies with genre " + genre);
-                        System.out.println("Use '|' character to split genres");
-                        return;
-                    }
-                    if (genre.length() > 0) 
-                        is_empty = false;
 
-                }
-                if (is_empty) {
-                    System.out.println("Please enter valid input");
-                    return;
-                }
-                //END
-            }
-            //-------------------------------------------------------------------
-            //toLowerCase may fail for ! character? or \?
-            //-------------------------------------------------------------------
-            try {
-                Integer occup_id = workID.get(work);
-                //START CHECK FOR OCCUPATION INPUT
-                if (occup_id == null) {
-                    //either some wrong string, or other type of occupation
-                    if (work.length() > 50) {
-                        System.out.println("There is no such occupation (name is too long)");
-                        return;
-                    } else {
-                        occup_id = 0;
-                        System.out.println("Since we can't recognize occupation " + work + ", we will regard it as 'other'");
-                    }
-                }
-                //END
-                //Elkhan's code
-                ArrayList<Integer> userID = Utils.getUsers(work, occup_id, age, gender);
-                // To ignore work -> call getUsers("", occup_id, age, gender);
-                // To ignore age -> call getUsers(work, occup_id, -1, gender);
-                // To ignore gender -> call getUsers(work, occup_id, age, "");
-                ArrayList<Integer> userIDNoWork = Utils.getUsers("", occup_id, age, gender);
-                HashMap<Integer, String> movies = args.length == 4 ? Utils.getMovies(genres) : Utils.getMovies();
-                Collections.sort(userID);
-                Collections.sort(userIDNoWork);
-                ArrayList<Integer> movieID = new ArrayList<>(movies.keySet());
-                //Check if movieID is empty
-                if (movieID.size() <= 0) {
-                    System.out.println("No movie found that satisfies requested genres: " + args[0]);
-                    return;
-                }
+        //* Gender check
+        gender = args[0].toLowerCase();
+        if (gender.compareTo("") != 0 // if gender is passed
+                && // and not equal to neither F nor M
+                !(gender.compareTo("F") == 0 || gender.compareTo("M") == 0)) {
 
-                Collections.sort(movieID);
-                HashMap<Integer, Integer[]> ratings = Utils.getRatings(userID, movieID);
-                //Elkhan's code
-                int count = 0;
-                HashMap<Integer, Double> scores = new HashMap<>();
-                if (ratings.size() < 10) {
-                    // System.out.println("The algorithm cannot recommend 10 movies, it will be less");
-                    HashMap<Integer, Integer[]> secondRatings = Utils.getRatings(userIDNoWork, movieID);
-                    secondRatings.keySet().removeAll(ratings.keySet());
-                    ratings.putAll(secondRatings);
-                    if (ratings.size() < 10)
-                        System.out.println("less");
-                }
-
-                for (Integer k : ratings.keySet()) {
-                    scores.put(k, (double) ratings.get(k)[0] / (double) ratings.get(k)[1]);
-                }
-                HashMap<Integer, Double> sortedScores = scores.entrySet().stream()
-                        .sorted(Comparator.comparingDouble(e -> -e.getValue()))
-                        .collect(Collectors.toMap(
-                                Map.Entry::getKey,
-                                Map.Entry::getValue,
-                                (a, b) -> {
-                                    throw new AssertionError();
-                                },
-                                LinkedHashMap::new
-                        ));
-
-                for (Integer k : sortedScores.keySet()) {
-                    Utils.printMovie(k, movies);
-                    count++;
-                    if (count == 10) break;
-                }
-            } catch (IOException e) {
-                // todo: Proper error handling
-                System.out.println("Some error happened");
-                e.printStackTrace();
-            } catch (NullPointerException e) {
-                // todo: Find the source of a bug
-                System.out.println("Null pointer exception somewhere");
-                e.printStackTrace();
-            }
-
+            System.out.println("Please, provide a proper arguement for Gender");
+            System.out.println("It shall be empty - \"\", male - \"M\" or female \"F\"");
+            return;
         }
+
+        //* Age check
+        if (args[1].length() > 0) {
+            for (int i = 0; i < args[1].length(); i++) {
+                if (!Character.isDigit(args[1].charAt(i))) {
+                    System.out.println("Please, enter a valid argument for age!");
+                    System.out.println("It should be a positive integer, containing only digits!");
+                    System.out.printf("Age shall not exceed %d\n", Integer.MAX_VALUE);
+                    return;
+                }
+            }
+            age = Integer.parseInt(args[1]);
+        }
+        else age = -1;
+
+        if (args.length == 4) {
+            genres = args[3].toLowerCase().split("\\|");
+            
+            //* Genres check
+            if (genres.length == 0) {
+                System.out.println("Please enter valid input");
+                System.out.println("Genres field should not be empty!");
+                return;
+            }
+            else if (genres.length > 10) {
+                System.out.println("Input for genres is too long, please try to include each genre only once.");
+                return;
+            }
+            boolean is_empty = true;
+            for (String genre : genres) {
+                if (genre.length() > 50) {
+                    System.out.println("There are no movies with genre " + genre);
+                    System.out.println("Use '|' character to split genres");
+                    return;
+                }
+                if (genre.length() > 0) is_empty = false;
+            }
+            if (is_empty) {
+                System.out.println("Please enter valid input");
+                return;
+            }
+        }
+        //-------------------------------------------------------------------
+        //! toLowerCase may fail for ! character? or \?
+        //-------------------------------------------------------------------
+        try {
+            work = args[2].toLowerCase();
+            Integer occup_id = workID.get(work);
+            //* Occupation check
+            if (occup_id == null) {
+                //either some wrong string, or other type of occupation
+                if (work.length() > 50) {
+                    System.out.println("There is no such occupation (name is too long)");
+                    return;
+                } 
+                else {
+                    occup_id = 0;
+                    System.out.println("Since we can't recognize occupation " + work + ", we will regard it as 'other'");
+                }
+            }
+            
+            //* Here, real implementation begins
+            ArrayList<Integer> userID = Utils.getUsers(work, occup_id, age, gender);
+            // To ignore work -> call getUsers("", occup_id, age, gender);
+            // To ignore age -> call getUsers(work, occup_id, -1, gender);
+            // To ignore gender -> call getUsers(work, occup_id, age, "");
+            ArrayList<Integer> userIDNoWork = Utils.getUsers("", occup_id, age, gender);
+            HashMap<Integer, String> movies = args.length == 4 ? Utils.getMovies(genres) : Utils.getMovies();
+            Collections.sort(userID);
+            Collections.sort(userIDNoWork);
+            ArrayList<Integer> movieID = new ArrayList<>(movies.keySet());
+            //Check if movieID is empty
+            if (movieID.size() <= 0) {
+                System.out.println("No movie found that satisfies requested genres: " + args[0]);
+                return;
+            }
+            Collections.sort(movieID);
+            HashMap<Integer, Integer[]> ratings = Utils.getRatings(userID, movieID);
+            //Elkhan's code
+            int count = 0;
+            HashMap<Integer, Double> scores = new HashMap<>();
+            if (ratings.size() < 10) {
+                // System.out.println("The algorithm cannot recommend 10 movies, it will be less");
+                HashMap<Integer, Integer[]> secondRatings = Utils.getRatings(userIDNoWork, movieID);
+                secondRatings.keySet().removeAll(ratings.keySet());
+                ratings.putAll(secondRatings);
+                if (ratings.size() < 10)
+                    System.out.println("less");
+            }
+            for (Integer k : ratings.keySet()) {
+                scores.put(k, (double) ratings.get(k)[0] / (double) ratings.get(k)[1]);
+            }
+            HashMap<Integer, Double> sortedScores = scores.entrySet().stream()
+                    .sorted(Comparator.comparingDouble(e -> -e.getValue()))
+                    .collect(Collectors.toMap(
+                            Map.Entry::getKey,
+                            Map.Entry::getValue,
+                            (a, b) -> {
+                                throw new AssertionError();
+                            },
+                            LinkedHashMap::new
+                    ));
+            for (Integer k : sortedScores.keySet()) {
+                Utils.printMovie(k, movies);
+                count++;
+                if (count == 10) break;
+            }
+        }
+        
+        //* Developer's helpers
+        catch (IOException e) {
+            // todo: Proper error handling
+            System.out.println("Some error happened");
+            e.printStackTrace();
+        } 
+        catch (NullPointerException e) {
+            // todo: Find the source of a bug
+            System.out.println("Null pointer exception somewhere");
+            e.printStackTrace();
+        }
+        
     }
 }
