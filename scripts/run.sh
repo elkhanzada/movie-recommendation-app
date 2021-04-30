6d2a4d33d4750d4c13a8a39837091c25192c1a29
Elkhan I <ismayilzadeelxan@gmail.com>
2021-04-30 12:13:41 +0900

    run.sh for milestone 2

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/elkhan199/group9.git
 cd group9
 mvn install
-java -cp target/group9-1.0-SNAPSHOT.jar Main Adventure educator
+java -cp target/group9-1.0-SNAPSHOT.jar Main "" "" ""
+java -cp target/group9-1.0-SNAPSHOT.jar Main "" "" "" "Adventure"
