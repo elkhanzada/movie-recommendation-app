515aa0446cbd094ac00346f2a53d95a0c62d6057
elkhan199 <ismayilzadeelxan@gmail.com>
2021-05-24 21:01:05 +0900

    updated README and run.sh

--- src/test/java/com/main/MainTest.java
+++ src/test/java/com/main/MainTest.java
@@ -1,23 +1,36 @@
+package com.main;
+
 import com.main.Main;
-import com.main.MainController;
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-import java.sql.SQLOutput;
 
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class MainTest {
+    @Autowired
+    private MockMvc mvc;
     @Test
     public void testMoreArgs(){
         try{
-            MainController mainController = new MainController();
-            String result = mainController.getMovies("{gender: 'F'}");
+
+            this.mvc.perform(get("/movies/recommendations")
+            .accept("{title: Toy Story (1994)}"))
+            .andDo(print());
+
         }catch (Exception e){
-            System.out.println(e.toString());
+            e.printStackTrace();
         }
     }
     @Test
