08b360e2f07013f831b2aa56ba48ed82fd9ed07d
elkhan199 <ismayilzadeelxan@gmail.com>
2021-06-02 01:16:32 +0900

    change of functions to use mongodb

--- src/main/java/com/utils/Utils.java
+++ src/main/java/com/utils/Utils.java
@@ -1,5 +1,6 @@
 package com.utils;
 
+import com.main.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -13,34 +14,32 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Utils {
-    public static JSONArray getTopN(ArrayList<ArrayList<Integer>> userLists, ArrayList<Integer> movieID, HashMap<Integer, String[]> movies, int n) throws IOException {
+    public static JSONArray getTopN(List<List<User>> userLists, List<Movie> movies, RatingDAL ratingDAL, int n) throws IOException {
         int count = 0;
         int index = 0;
         ArrayList<Integer> printedList = new ArrayList<>();
-        JSONArray jsonArray = new JSONArray();
+        JSONArray movieArray = new JSONArray();
         while (count < n) {
             int allvotes = 0;
             double totalMean = 0;
             if (index >= userLists.size()) break;
-            ArrayList<Integer> list = userLists.get(index);
-            Collections.sort(list);
-            HashMap<Integer, Integer[]> ratings = Utils.getRatings(list, movieID);
+            List<User> list = userLists.get(index);
+            List<Rating> ratings = Utils.getRatings(list,movies,ratingDAL);
             HashMap<Integer, Double> scores = new HashMap<>();
-            for (Integer k : ratings.keySet()) {
-                allvotes += ratings.get(k)[1];
-                totalMean += (double) ratings.get(k)[0] / (double) ratings.get(k)[1];
+            for (Rating rating: ratings) {
+                allvotes += rating.getVotes();
+                totalMean += rating.getAverage();
             }
             if (allvotes != 0)
                 totalMean /= allvotes;
-            for (Integer k : ratings.keySet()) {
-                scores.put(k, weightedRating((double) ratings.get(k)[0] / (double) ratings.get(k)[1],
-                        (double) ratings.get(k)[1],
+            for (Rating rating: ratings) {
+                scores.put(rating.getMovieId(), weightedRating(rating.getAverage(),
+                        rating.getVotes(),
                         10,
                         totalMean));
             }
             LinkedHashMap<Integer, Double> sortedScores = scores.entrySet().stream()
                     .sorted(Comparator.comparingDouble(e -> -e.getValue()))
-//                    .sorted(Comparator.comparingDouble(e -> -e.getValue()[1]))
                     .collect(Collectors.toMap(
                             Map.Entry::getKey,
                             Map.Entry::getValue,
@@ -56,102 +55,47 @@ public class Utils {
                         continue;
                     else
                         printedList.add(k);
-//                    System.out.print("Average Score: "+sortedScores.get(k)[0]+ " Number of users: "+sortedScores.get(k)[1] + " --------> ");
-                    jsonArray.put(Utils.printMovie(k, movies));
+                    movieArray.put(new JSONObject(Utils.getMovieDetails(k, movies)));
                     count++;
                 }
                 if (count >= n) break;
             }
             index++;
         }
-        return jsonArray;
+        return movieArray;
     }
 
     public static double weightedRating(double R, double v, double m, double C) {
         return (R * v + C * m) / (v + m);
     }
 
-    public static JSONObject printMovie(Integer chosenMovie, HashMap<Integer, String[]> movies) throws IOException {
-        BufferedReader scan = new BufferedReader(new FileReader(new File("data/links.dat")));
-        String line;
-        JSONObject jsonObject = new JSONObject();
-        while ((line = scan.readLine()) != null) {
-            int movieID = Integer.parseInt(line.split("::")[0]);
-            if (chosenMovie == movieID) {
-//                System.out.println(movies.get(chosenMovie) + " ( http://www.imdb.com/title/tt" + line.split("::")[1] + " )");
-                jsonObject.put("title", movies.get(chosenMovie)[0]);
-                jsonObject.put("genre", movies.get(chosenMovie)[1]);
-                jsonObject.put("imdb", "http://www.imdb.com/title/tt" + line.split("::")[1]);
-                return jsonObject;
-            }
+    public static Movie getMovieDetails(Integer chosenMovie, List<Movie> movies){
+        for(Movie movie: movies){
+            if(movie.getMovieID()==chosenMovie)
+                return movie;
         }
         return null;
     }
 
-    public static ArrayList<ArrayList<Integer>> getAllUsers(String work, Integer occupation, Integer age, String gender) throws IOException {
-        ArrayList<ArrayList<Integer>> lists = new ArrayList<>();
-        lists.add(getUsers(work, occupation, age, gender));
-        lists.add(getUsers("", occupation, age, gender));
-        lists.add(getUsers(work, occupation, age, ""));
-        lists.add(getUsers(work, occupation, -1, gender));
-        lists.add(getUsers("", occupation, age, ""));
-        lists.add(getUsers(work, occupation, -1, ""));
-        lists.add(getUsers("", occupation, -1, gender));
-        lists.add(getUsers("", occupation, -1, ""));
+    public static List<List<User>> getAllUsers(String work, Integer occupation, Integer age, String gender, UserDAL userDAL) throws IOException {
+        List<List<User>> lists = new ArrayList<>();
+        lists.add(userDAL.getSpecificUsers(occupation, age, gender));
+        lists.add(userDAL.getSpecificUsers(occupation, age, gender));
+        lists.add(userDAL.getSpecificUsers(occupation, age, ""));
+        lists.add(userDAL.getSpecificUsers(occupation, -1, gender));
+        lists.add(userDAL.getSpecificUsers(-1, age, ""));
+        lists.add(userDAL.getSpecificUsers(occupation, -1, ""));
+        lists.add(userDAL.getSpecificUsers(-1, -1, gender));
+        lists.add(userDAL.getSpecificUsers(-1, -1, ""));
         return lists;
     }
 
-    public static HashMap<Integer, Integer[]> getRatings(ArrayList<Integer> userID, ArrayList<Integer> movieID) throws IOException {
+    public static List<Rating> getRatings(List<User>users,List<Movie> movies, RatingDAL ratingDAL) {
         // ! --ratings.dat--
         // UserID::MovieID::Rating::Timestamp
-        BufferedReader scan = new BufferedReader(new FileReader(new File("data/ratings.dat")));
-        int i = 0;
-        int j = 0;
-        String line;
-        HashMap<Integer, Integer[]> ratingList = new HashMap<>();
-        while ((line = scan.readLine()) != null) {
-            String[] rating = line.split("::");
-            // Collections.binarySearch() returns a negative number if the item not found;
-            i = Collections.binarySearch(userID, Integer.parseInt(rating[0]));
-            j = Collections.binarySearch(movieID, Integer.parseInt(rating[1]));
-            if (i > -1 && j > -1) { // if we find corresponding movie and user;
-                if (!ratingList.containsKey(Integer.parseInt(rating[1])))
-                    ratingList.put(Integer.parseInt(rating[1]), new Integer[]{Integer.parseInt(rating[2]), 1});
-                else
-                    ratingList.put(Integer.parseInt(rating[1]), new Integer[]{ratingList.get(Integer.parseInt(rating[1]))[0] + Integer.parseInt(rating[2]), ratingList.get(Integer.parseInt(rating[1]))[1] + 1});
-            }
-        }
 
-        scan.close();
-        return ratingList;
+        return ratingDAL.getSpecificRatings(users,movies);
     }
-
-    public static ArrayList<Integer> getUsers(String work, Integer occupation, Integer age, String gender) throws IOException {
-        // ! --users.dat--
-        // UserID::Gender::Age::Occupation::Zip-code
-        BufferedReader scan = new BufferedReader(new FileReader(new File("data/users.dat")));
-        ArrayList<Integer> list = new ArrayList<>();
-        HashMap<Integer, Integer[]> agelist = new HashMap<>();
-        agelist.put(1, new Integer[]{0, 17});
-        agelist.put(18, new Integer[]{18, 24});
-        agelist.put(25, new Integer[]{25, 34});
-        agelist.put(35, new Integer[]{35, 44});
-        agelist.put(45, new Integer[]{45, 49});
-        agelist.put(50, new Integer[]{50, 55});
-        agelist.put(56, new Integer[]{56, Integer.MAX_VALUE});
-        String line;
-        while ((line = scan.readLine()) != null) {
-            String[] user = line.split("::");
-            if ((Integer.parseInt(user[3]) == occupation || work.length() == 0) &&
-                    ((age == -1) || (agelist.get(Integer.parseInt(user[2]))[0] <= age &&
-                            agelist.get(Integer.parseInt(user[2]))[1] >= age)) &&
-                    (gender.length() == 0 || gender.toLowerCase().equals(user[1].toLowerCase())))
-                list.add(Integer.parseInt(user[0]));
-        }
-        scan.close();
-        return list;
-    }
-
     // This function returns genres of the provided movie
     public static String[] getGenres(String movieName) throws IllegalArgumentException, IOException {
         BufferedReader scan = new BufferedReader(new FileReader(new File("data/movies.dat")));
@@ -162,7 +106,7 @@ public class Utils {
         while ((line = scan.readLine()) != null) {
             String[] movies = line.split("::");
             if (movieName.equals(movies[1].toLowerCase())) {
-                result = movies[2].split("\\|");
+                result = movies[2].toLowerCase().split("\\|");
                 found = true;
                 break;
             }
@@ -175,26 +119,11 @@ public class Utils {
     }
 
     // This function returns movieID-s with matching genres
-    public static HashMap<Integer, String[]> getMovies(String[] genres, String exclude) throws IOException {
+    public static List<Movie> getMovies(String[] genres, String exclude, MovieDAL movieDAL) throws IOException {
         // ! --movies.dat--
         // MovieID::Title::Genres
-        BufferedReader scan = new BufferedReader(new FileReader(new File("data/movies.dat")));
-        HashMap<Integer, String[]> list = new HashMap<>();
-        String line;
-        while ((line = scan.readLine()) != null) {
-            String[] movie = line.split("::");
-            String[] genres_list = movie[2].split("\\|");
-            for (String s : genres) {
-                for (String g : genres_list) {
-                    if (s.toLowerCase().equals(g.toLowerCase())) {
-                        if (movie[1].toLowerCase().equals(exclude)) continue;
-                        list.put(Integer.parseInt(movie[0]), new String[]{movie[1], movie[2]});
-                    }
-                }
-            }
-        }
-        scan.close();
-        return list;
+
+        return movieDAL.getSpecificMovies(genres,exclude);
     }
 
     public static HashMap<Integer, String[]> getMovies(String exclude) throws IOException {
