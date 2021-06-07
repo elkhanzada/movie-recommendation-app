80a08cfd73de8b8a12d6ec44827afc4e99476ddd
elkhan199 <ismayilzadeelxan@gmail.com>
2021-06-07 20:16:21 +0900

    remove duplicate code

--- src/main/resources/public/index.js
+++ src/main/resources/public/index.js
@@ -12,16 +12,19 @@ $(document).ready(function() {
             $("body").append(main)
         }
     }
+    function startBuilding(res) {
+        $(".card").remove();
+        $.each(res, function(key,value) {
+            makeCards(value)
+        });
+    }
     $('#users').submit(function() { // catch the form's submit event
         $.ajax({ // create an AJAX call...
             data: $(this).serialize(), // get the form data
             type: $(this).attr('method'), // GET or POST
             url: $(this).attr('action'), // the file to call
             success: function(res) { // on success..
-                $(".card").remove();
-                $.each(res, function(key,value) {
-                    makeCards(value)
-                });
+                startBuilding(res)
             }
         });
         return false; // cancel original event to prevent form submitting
@@ -32,10 +35,7 @@ $(document).ready(function() {
             type: $(this).attr('method'), // GET or POST
             url: $(this).attr('action'), // the file to call
             success: function(res) { // on success..
-                $(".card").remove();
-                $.each(res, function(key,value) {
-                    makeCards(value)
-                });
+                startBuilding(res)
             }
         });
         return false; // cancel original event to prevent form submitting
