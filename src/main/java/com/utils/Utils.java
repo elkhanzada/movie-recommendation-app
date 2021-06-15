7af33ab0d883ffad0fc4d19d567821eb39a5c299
elkhan199 <ismayilzadeelxan@gmail.com>
2021-06-15 17:55:58 +0900

    getusers modified

--- src/main/java/com/utils/Utils.java
+++ src/main/java/com/utils/Utils.java
@@ -87,17 +87,41 @@ public class Utils {
     public static List<List<User>> getAllUsers(Integer occupation, Integer age, String gender, UserDAL userDAL) throws IOException {
         List<List<User>> lists = new ArrayList<>();
         if(occupation==-1&&age==-1&&gender.equals("")) {
+            lists.add(userDAL.getSpecificUsers(-1, -1, ""));
+        }else if(occupation==-1&&age==-1){
+            lists.add(userDAL.getSpecificUsers(-1, -1, gender));
+            lists.add(userDAL.getSpecificUsers(-1, -1, ""));
+        }else if(age==-1&&gender.equals("")){
+            lists.add(userDAL.getSpecificUsers(occupation, -1, ""));
+            lists.add(userDAL.getSpecificUsers(-1, -1, ""));
+        }else if(occupation==-1&&gender.equals("")){
+            lists.add(userDAL.getSpecificUsers(-1, age, ""));
+            lists.add(userDAL.getSpecificUsers(-1, -1, ""));
+        }else if(occupation==-1){
+            lists.add(userDAL.getSpecificUsers(-1, age, gender));
+            lists.add(userDAL.getSpecificUsers(-1, age, ""));
+            lists.add(userDAL.getSpecificUsers(-1, -1, gender));
+            lists.add(userDAL.getSpecificUsers(-1, -1, ""));
+        }else if(age==-1){
+            lists.add(userDAL.getSpecificUsers(occupation, -1, gender));
+            lists.add(userDAL.getSpecificUsers(-1, -1, gender));
+            lists.add(userDAL.getSpecificUsers(occupation, -1, ""));
+            lists.add(userDAL.getSpecificUsers(-1, -1, ""));
+        }else if(gender.equals("")){
+            lists.add(userDAL.getSpecificUsers(occupation, age, ""));
+            lists.add(userDAL.getSpecificUsers(-1, age, ""));
+            lists.add(userDAL.getSpecificUsers(occupation, -1, ""));
+            lists.add(userDAL.getSpecificUsers(-1, -1, ""));
+        }else {
             lists.add(userDAL.getSpecificUsers(occupation, age, gender));
-            return lists;
+            lists.add(userDAL.getSpecificUsers(-1, age, gender));
+            lists.add(userDAL.getSpecificUsers(occupation, age, ""));
+            lists.add(userDAL.getSpecificUsers(occupation, -1, gender));
+            lists.add(userDAL.getSpecificUsers(-1, age, ""));
+            lists.add(userDAL.getSpecificUsers(occupation, -1, ""));
+            lists.add(userDAL.getSpecificUsers(-1, -1, gender));
+            lists.add(userDAL.getSpecificUsers(-1, -1, ""));
         }
-        lists.add(userDAL.getSpecificUsers(occupation, age, gender));
-        lists.add(userDAL.getSpecificUsers(-1, age, gender));
-        lists.add(userDAL.getSpecificUsers(occupation, age, ""));
-        lists.add(userDAL.getSpecificUsers(occupation, -1, gender));
-        lists.add(userDAL.getSpecificUsers(-1, age, ""));
-        lists.add(userDAL.getSpecificUsers(occupation, -1, ""));
-        lists.add(userDAL.getSpecificUsers(-1, -1, gender));
-        lists.add(userDAL.getSpecificUsers(-1, -1, ""));
         return lists;
     }
 
