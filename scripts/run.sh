cc6a5fd7dec5c7676de5ee5da20e844aacda9849
Elkhan <ismayilzadeelxan@gmail.com>
2021-09-18 21:40:11 +0900

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/elkhan199/group9.git
+git clone https://github.com/elkhanzada/movie-recommendation-app.git
 cd group9
 mvn package
 java -jar target/group9-1.0-SNAPSHOT.jar
